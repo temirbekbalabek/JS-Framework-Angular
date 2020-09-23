9629c94c3d7804b9e1df8c355f1cc7b995d854a5
Temirbek <temirbek.balabek@mail.ru>
2020-09-24 02:46:01 +0600

    third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
-import { OperateComponent } from './components/operate/operate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    OperateComponent
   ],
   imports: [
     BrowserModule,
